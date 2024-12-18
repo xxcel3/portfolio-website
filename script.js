208c1c942785551ff59d55c0b9612985cb217d68
Eve Lin <evelin@buffalo.edu>
2024-12-18 01:24:52 -0500

    escape html

--- script.js
+++ script.js
@@ -7,37 +7,36 @@ function closemenu() {
     sidemenu.style.right = "-200px";
 }
 
-// contact form 
-const scriptURL = 'https://script.google.com/macros/s/AKfycbwUdc1p0SRdFwn6tIT0op5f-H6Ia2WRRkVEHEf-UuQRjhsj9uw-_30jRAyR3ZKAJW86/exec';
-const form = document.forms['submit-to-google-sheet'];
-const msg = document.getElementById("msg");
-
 function escapeHTML(input) {
     const div = document.createElement('div');
     div.textContent = input;
     return div.innerHTML;
 }
 
+// contact form
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwUdc1p0SRdFwn6tIT0op5f-H6Ia2WRRkVEHEf-UuQRjhsj9uw-_30jRAyR3ZKAJW86/exec';
+const form = document.forms['submit-to-google-sheet'];
+const msg = document.getElementById("msg");
+
 form.addEventListener('submit', e => {
     e.preventDefault();
 
     // sanitize 
-    const sanitizedName = escapeHTML(form.Name.value);
-    const sanitizedEmail = escapeHTML(form.Email.value);
-    const sanitizedMessage = escapeHTML(form.Message.value);
-
     const sanitizedFormData = new FormData();
-    sanitizedFormData.append('Name', sanitizedName);
-    sanitizedFormData.append('Email', sanitizedEmail);
-    sanitizedFormData.append('Message', sanitizedMessage);
+    sanitizedFormData.append('Name', escapeHTML(form.Name.value));
+    sanitizedFormData.append('Email', escapeHTML(form.Email.value));
+    sanitizedFormData.append('Message', escapeHTML(form.Message.value));
 
     fetch(scriptURL, { method: 'POST', body: sanitizedFormData })
         .then(response => {
             msg.innerHTML = "Message sent :) Thank you!";
-            setTimeout(function () {
+            setTimeout(() => {
                 msg.innerHTML = "";
             }, 5000);
             form.reset();
         })
-        .catch(error => console.error('Error!', error.message));
+        .catch(error => {
+            console.error('Error!', error.message);
+            msg.innerHTML = "Failed to send message. Please try again.";
+        });
 });
