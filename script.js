43dd497a82a1bbfb007de78412e7e5047076dc1e
Eve Lin <evelin@buffalo.edu>
2024-12-18 03:05:00 -0500

    form update

--- script.js
+++ script.js
@@ -10,22 +10,6 @@ function closemenu() {
 }
 
 // contact form
-const scriptURL = 'https://script.google.com/macros/s/AKfycbwUdc1p0SRdFwn6tIT0op5f-H6Ia2WRRkVEHEf-UuQRjhsj9uw-_30jRAyR3ZKAJW86/exec'
-const form = document.forms['submit-to-google-sheet']
-const msg = document.getElementById("msg")
-
-form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-        .then(response => {
-            msg.innerHTML = "Message sent :) Thank you!"
-            setTimeout(function(){
-                msg.innerHTML = ""
-            },5000)
-            form.reset()
-        })
-        .catch(error => console.error('Error!', error.message))
-})
 
 // portfilo
 function toggleDetails(projectId) {
